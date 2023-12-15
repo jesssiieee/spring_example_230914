acdac54561eb5360821f519809dee52d1457080d
test <test@test.com>
2023-12-15 18:43:08 +0900

    lesson02 / lesson03.ex01

--- src/main/java/com/example/lesson02/bo/UsedGoodsBO.java
+++ src/main/java/com/example/lesson02/bo/UsedGoodsBO.java
@@ -2,17 +2,22 @@ package com.example.lesson02.bo;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.lesson02.domain.UsedGoods;
+import com.example.lesson02.mapper.UsedGoodsMapper;
 
 @Service // Business Object에 관한 Spring bean 등록
 public class UsedGoodsBO {
 	
+	@Autowired // Dependency Injection(DI): 의존성 주입
+	private UsedGoodsMapper usedGoodsMapper; // Spring Bean을 가져온다.
+	
 	// input : X (ex)DB의 where절 <- 컨트롤러에게 받아온다		
 	// output : (Repository에게 받은 것을) List<UsedGoods> (컨트롤러에게 전달)
 	public List<UsedGoods> getUsedGoodsList() {
-		return ;
+		return usedGoodsMapper.selectUsedGoodsList();
 	}
 	
 }
